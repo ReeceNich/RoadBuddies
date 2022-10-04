4b15aa8d55d7b34531ecea7cdca96af91e57899c
Reece Nicholls <33102368+ReeceNich@users.noreply.github.com>
2022-10-04 19:16:54 +0100

    Initial Commit

---
+++ Driving Behaviours/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Driving Behaviours
+//
+//  Created by Reece Nicholls on 04/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Driving Behaviours/Driving_BehavioursApp.swift
@@ -0,0 +1,17 @@
+//
+//  Driving_BehavioursApp.swift
+//  Driving Behaviours
+//
+//  Created by Reece Nicholls on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Driving_BehavioursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
